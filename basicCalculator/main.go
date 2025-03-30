ad76a4a687eccfc54dff9e383f17a26d8888a697
danielnicolae28 <danielnicolae286@gmail.com>
2025-03-30 11:18:11 +0300

    first project basic Calculator

--- basicCalculator/main.go
+++ basicCalculator/main.go
@@ -3,5 +3,52 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	var num1, num2, result float64
+	var operation string
+	var storeResult []float64
+
+	fmt.Println("Basic Calculator")
+	for {
+
+		fmt.Println("Enter first number :")
+		fmt.Scan(&num1)
+		fmt.Println("Enter second number :")
+		fmt.Scan(&num2)
+
+		fmt.Println("Choose the operation (+,-,*,/) : ")
+		fmt.Scan(&operation)
+
+		switch operation {
+		case "+":
+			result = num1 + num2
+			storeResult = append(storeResult, result)
+			fmt.Printf("Sum result : %v\n", result)
+		case "-":
+			result = num1 - num2
+			storeResult = append(storeResult, result)
+			fmt.Printf("Substraction result : %v\n", result)
+		case "*":
+			result = num1 * num2
+			storeResult = append(storeResult, result)
+			fmt.Printf("Multiplication result : %v\n", result)
+		case "/":
+			result = num1 / num2
+			storeResult = append(storeResult, result)
+			fmt.Printf("Devision result : %v\n", result)
+		}
+
+		fmt.Printf("Result history, %v\n", storeResult)
+
+		var response string
+		fmt.Println("Would you want another calculation : yes or no")
+		fmt.Scan(&response)
+		if response == "yes" || response == "YES" || response == "Yes" {
+			continue
+		} else {
+			break
+		}
+
+	}
+
 }
